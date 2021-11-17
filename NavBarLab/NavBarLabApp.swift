b790bf76ac7ba89c46697f25df0156d82e01ba7d
Konstantin Kostov <konstantin@headbright.be>
2021-11-17 08:41:37 +0100

    Initial Commit

---
+++ NavBarLab/NavBarLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavBarLabApp.swift
+//  NavBarLab
+//
+//  Created by Konstantin Kostov on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavBarLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
